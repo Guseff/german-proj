731e3ce521068b05338c190b41a9fdd3ebaccab0
Vasily Gusev <vasily.guseff@gmail.com>
2020-07-29 21:31:49 +0300

    refactor: change App structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
+import 'antd/dist/antd.css'
 import './assets/styles/index.css'
-import 'bootstrap/dist/css/bootstrap.css'
 
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
+import { Layout } from 'antd'
+
+import University from './components/Footer'
+import Start from './components/Start'
+import Media from './components/Media'
+import NoMatch from './components/NoMatch'
+
+const { Footer } = Layout
 
 const App = () => {
-  return <div />
+  return (
+    <Layout className="main-wrapper">
+      <Layout className="page-wrapper light">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Start} />
+            <Route path="/media/:id" component={Media} />
+            <Route path="/404" component={NoMatch} />
+            <Route path="*">
+              <Redirect to="/404" />
+            </Route>
+          </Switch>
+        </Router>
+      </Layout>
+      <Footer className="light">
+        <University />
+      </Footer>
+    </Layout>
+  )
 }
 
 export default App
