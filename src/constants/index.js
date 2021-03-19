7f674e9d93bdc31645e63e7f027804922b18f2c3
Vasily Gusev <vasily.guseff@gmail.com>
2021-03-19 21:48:39 +0300

    three videos works

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,3 +10,12 @@ export const TENSES = {
   PRETER: 'preter',
   DEKLINATION: 'deklination',
 };
+
+export const VIDEO_SRC = {
+  'ru-prasens-vid1':
+    'https://drive.google.com/uc?export=download&id=1wUNfr8F0PCkHqR-S-ASUFMw5riTPoxa-',
+  'de-prasens-vid1':
+    'https://drive.google.com/uc?export=download&id=1dx4C_JF13loEKN3z0JlioOteAQUUazkE',
+  'en-prasens-vid1':
+    'https://drive.google.com/uc?export=download&id=17EaWhb3dmRcwYaUINCobIKMI-r1_hPZC',
+};
--- src/views/Media.jsx
+++ src/views/Media.jsx
@@ -7,20 +7,13 @@ import PageTemplate from '../components/common/PageTemplate';
 import TenseMenu from '../components/common/TenseMenu';
 import IdMenu from '../components/common/IdMenu';
 import { LangContext } from '../context';
-import { TENSES } from '../constants';
-
-const srcMap = {
-  'ru-prasens-vid1':
-    'https://drive.google.com/file/d/1wUNfr8F0PCkHqR-S-ASUFMw5riTPoxa-/view?usp=sharing',
-  'de-prasens-vid1':
-    'https://drive.google.com/file/d/1dx4C_JF13loEKN3z0JlioOteAQUUazkE/view?usp=sharing',
-};
+import { TENSES, VIDEO_SRC } from '../constants';
 
 const Media = () => {
   const { lang } = useContext(LangContext);
   const { tense, id } = useParams();
   const srcKey = `${lang}-${tense}-vid${id}`;
-  const src = srcMap[srcKey];
+  const src = VIDEO_SRC[srcKey];
   const poster = `/posters/de-${tense}-vid${id}.png`;
 
   if (
@@ -36,7 +29,12 @@ const Media = () => {
       <Layout.Content className="page-content">
         <TenseMenu tense={tense} />
         <IdMenu tense={tense} id={id} />
-        <Player src={src} poster={poster} className="page-player">
+        <Player
+          src={src}
+          poster={poster}
+          className="page-player"
+          type="video/mp4"
+        >
           <BigPlayButton position="center" />
         </Player>
       </Layout.Content>
