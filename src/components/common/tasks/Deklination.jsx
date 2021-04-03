8e78e361bfc169d256204e5052f965fa4f186fd3
Vasili <vasili_huseu@epam.com>
2021-04-03 12:50:38 +0300

    add tests text

---
+++ src/components/common/tasks/Deklination.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DeklinationTest = () => {
+  return <section>Deklination tests are under construction</section>;
+};
+
+export default DeklinationTest;
--- src/views/Tests.jsx
+++ src/views/Tests.jsx
@@ -5,6 +5,12 @@ import { useParams, Redirect } from 'react-router-dom';
 import PageTemplate from '../components/common/PageTemplate';
 import TenseMenu from '../components/common/TenseMenu';
 import { TENSES } from '../constants';
+import {
+  DeklinationTest,
+  PerfectTest,
+  PrasensTest,
+  PreterTest,
+} from '../components/common/tasks';
 
 const Tests = () => {
   const { tense } = useParams();
@@ -17,10 +23,29 @@ const Tests = () => {
   )
     return <Redirect to="/404" />;
 
+  let TestComponent;
+  switch (tense) {
+    case TENSES.PERFECT:
+      TestComponent = PerfectTest;
+      break;
+    case TENSES.PRASENS:
+      TestComponent = PrasensTest;
+      break;
+    case TENSES.PRETER:
+      TestComponent = PreterTest;
+      break;
+    case TENSES.DEKLINATION:
+      TestComponent = DeklinationTest;
+      break;
+    default:
+      throw new Error('unappropiate tense');
+  }
+
   return (
     <PageTemplate>
       <Layout.Content className="page-content">
         <TenseMenu tense={tense} component="tests" />
+        <TestComponent />
       </Layout.Content>
     </PageTemplate>
   );
