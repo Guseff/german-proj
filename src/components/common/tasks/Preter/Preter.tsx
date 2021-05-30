8ce6f0f02e2a8f43d8ebaddf16793dd7e8b97d9c
Vasili <vasili_huseu@epam.com>
2021-05-30 21:00:25 +0300

    add task elements for all tasks

--- src/components/common/tasks/Preter/Preter.tsx
+++ src/components/common/tasks/Preter/Preter.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-irregular-whitespace */
 import React from 'react';
+import { Task1, Task2, Task3, Task4, Task5, Task6, Task7 } from './components';
 
 const PreterTest = (): JSX.Element => {
   return (
@@ -8,150 +9,25 @@ const PreterTest = (): JSX.Element => {
         1) Bitte ergänzen Sie in den leeren Feldern die richtigen Verbformen.
         Die bereits eingetragenen Verbformen helfen Ihnen dabei:
       </h4>
-      <table width="625">
-        <tbody>
-          <tr>
-            <td width="14%">Infinitiv</td>
-            <td width="14%">ich</td>
-            <td width="14%">du</td>
-            <td width="14%">er/sie/es</td>
-            <td width="14%">wir</td>
-            <td width="14%">ihr</td>
-            <td width="14%">sie/Sie</td>
-          </tr>
-          <tr>
-            <td width="14%">legen</td>
-            <td width="14%"> </td>
-            <td width="14%"> </td>
-            <td width="14%">legte</td>
-            <td width="14%" />
-            <td width="14%"> </td>
-            <td width="14%"> </td>
-          </tr>
-          <tr>
-            <td width="14%">wohnen</td>
-            <td width="14%" />
-            <td width="14%"> wohntest</td>
-            <td width="14%"> </td>
-            <td width="14%"> </td>
-            <td width="14%"> </td>
-            <td width="14%"> </td>
-          </tr>
-          <tr>
-            <td width="14%">kochen</td>
-            <td width="14%"> </td>
-            <td width="14%"> </td>
-            <td width="14%" />
-            <td width="14%"> </td>
-            <td width="14%"> </td>
-            <td width="14%"> kochten</td>
-          </tr>
-          <tr>
-            <td width="14%">holen</td>
-            <td width="14%"> </td>
-            <td width="14%" />
-            <td width="14%"> </td>
-            <td width="14%">holten</td>
-            <td width="14%"> </td>
-            <td width="14%"> </td>
-          </tr>
-          <tr>
-            <td width="14%">küssen</td>
-            <td width="14%"> </td>
-            <td width="14%"> </td>
-            <td width="14%"> </td>
-            <td width="14%"> </td>
-            <td width="14%">küsstet</td>
-            <td width="14%"> </td>
-          </tr>
-          <tr>
-            <td width="14%">fragen</td>
-            <td width="14%">fragte</td>
-            <td width="14%"> </td>
-            <td width="14%"> </td>
-            <td width="14%"> </td>
-            <td width="14%"> </td>
-            <td width="14%"> </td>
-          </tr>
-        </tbody>
-      </table>
+      <Task1 />
       <h4>2) Setzen Sie die Verben in Klammern im Präteritum ein:</h4>
-      <ol>
-        <li>Er … die Stadt, wie Batman (retten)</li>
-        <li>Es … gestern den ganzen Tag (regnen)</li>
-        <li>Monika und Karl … von ihm nur mit mir (reden)</li>
-        <li>Lila … das Fenster (öffnen)</li>
-        <li>Stefan …. erleichtert …. (ausatmen)</li>
-      </ol>
+      <Task2 />
       <h4>3) Wählen Sie die richtige Variante:</h4>
-      <ol>
-        <li>
-          Er redetet/redete stundenlang über seine Erfahrung und konnte nicht
-          aufhören.
-        </li>
-        <li>
-          Damals ordnte/ordnete ich die Bücher immer, jetzt habe ich nicht genug
-          Zeit
-        </li>
-        <li>Die Menschen atmeten/atmten ruhig und sahen zu</li>
-        <li>Ich wusste/wisste das! Wie kannst du nur so offenbar lügen?</li>
-        <li>Früher zeichnetest/zeichnetst du gern, was ist passiert?</li>
-        <li>Du brachtest/bringtest mir diese Nachricht in der Nacht, warum?</li>
-      </ol>
+      <Task3 />
       <h4>4) Setzen Sie die Verben in Klammern im Präteritum ein:</h4>
-      <ol>
-        <li>Du … gestern fast nichts, war alles okay? (essen)</li>
-        <li>… ihr den Brief also langweilig? (finden)</li>
-        <li>
-          Früher ... wir uns die neuen Filme immer zusammen … . (sich ansehen)
-        </li>
-        <li>Du … in der Halle so stumm und traurig. Was ist los? (sitzen)</li>
-        <li>
-          Ihr … als das schönste Paar in der Schule, warum seid ihr nicht mehr
-          zusammen? (gelten)
-        </li>
-      </ol>
+      <Task4 />
       <h4>
         5) Wie lauten die Präteritumformen der folgenden starken Verben?
         Schreiben Sie auf
       </h4>
-      <p>
-        давать, приказывать, идти, ехать, побеждать, звать/кричать, плавать,
-        прыгать, стоять, носить
-      </p>
+      <Task5 />
       <h4>
         6) Übersetzen Sie ins Deutsche! Verwenden Sie die Verben aus den
         Klammern
       </h4>
-      <ol>
-        <li>Ты пил и пил, без конца (trinken)</li>
-        <li>Вчера у нас было всё, сегодня – ничего (haben)</li>
-        <li>Я был тогда учителем (sein)</li>
-        <li>Дети, вы должны были прийти раньше (müssen)</li>
-        <li>Они долго шли через лес (gehen)</li>
-      </ol>
+      <Task6 />
       <h4>7) Sezen Sie die eingeklammerten Verben im Präteritum ein</h4>
-      <p>
-        Der Monarch, …, … Friedrich August (heißen). Und er … der letzte
-        sächsische König (sein). Doch das … er damals noch nicht (wissen).
-        Manchmal … er mit seinen Kindern durch die Residenzstadt (fahren). Neben
-        dem Kutscher …, mit verschränkten Armen und einem schillernden Federhut,
-        der Leibjäger (sitzen). Und aus dem offenen Wagen … die kleinen Prinzen
-        und Prinzessinnen uns anderen Kindern (zuwinken). Der König … auch
-        (winken). Und er … freundlich (lächeln). …
-      </p>
-      <p>
-        Um die Weihnachtszeit … er manchmal, ganz allein und mit hochgestelltem
-        Mantelkragen, wie andere Offiziere auch, durch die abendlich funkelnde
-        Prager Straße und … nachdenklich vor den schimmernden Schaufenstern
-        (spazieren, stehen bleiben). Für Kinderkleider und Spielwaren … er am
-        meisten (sich interessieren). Es … (schneien). In den Läden … die
-        Christbäume (glitzern). Die Passanten … (sich anstoßen), … (flüstern):
-        »Der König!« und … eilig (weitergehen), um ihn nicht zu stören. Er …
-        einsam (sein). Er … seine Kinder (lieben). Und deshalb … ihn die
-        Bevölkerung (lieben). …
-      </p>
-      (Erich Kästner „Als ich ein kleiner Junge war“)
+      <Task7 />
     </section>
   );
 };
